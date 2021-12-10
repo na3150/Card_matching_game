b3efe68e73961e618c3459be80bfe85e2f258f77
성나영 <na3150@gmail.com>
2021-12-10 21:20:51 +0900

    점수판 만들기

--- card_matching_game.py
+++ card_matching_game.py
@@ -4,9 +4,10 @@ import random
 import time
 from tkinter import *
 
-#게임이 시작하기 전에 게임 룰을 설명해주는 것도 좋을 듯
 
-def find_card(x, y): #클릭한 카드 찾기
+# 게임이 시작하기 전에 게임 룰을 설명해주는 것도 좋을 듯
+
+def find_card(x, y):  # 클릭한 카드 찾기
     min_idx = 0
     min_dis = 100
     for i in range(16):
@@ -16,9 +17,11 @@ def find_card(x, y): #클릭한 카드 찾기
             min_idx = i
     return min_idx
 
-def score_updata(m):
+
+def score_update(m):
     score_pen.clear()
     score_pen.write(f"{m}  {score}점/{attempt}번 시도", False, "center", ("", 15))
+
 
 def result(m):
     t.goto(0, -60)
@@ -26,57 +29,78 @@ def result(m):
 
 
 def show_scoreboard():
-    player_list.sort() #순위 정렬
+    player_list.sort()  # 순위 정렬
+    root = Tk()
+    root.title("점수판")
+    root.geometry("300x70")
+    frame = tkinter.Frame(root)
+    frame.pack()
+    label_instruction = Label(root, text="점수는 성공까지의 시도 횟수 입니다.", width=300)
+    label_instruction.pack()
+    num = 1
+    for player in player_list:
+        label = Label(root, text=str(num) + "등 " + str(player[0]) + "점    닉네임: " + player[1], width=300)
+        label.pack()
+        num += 1
 
 
 def play(x, y):
-    global click_num #전역변수 사옹
+    global click_num  # 전역변수 사옹
     global first_pick
     global second_pick
     global attempt
     global score
     if attempt == 12:
-        result("Game Over") #게임 종료
+        result("Game Over")  # 게임 종료
         check_more_game()
     else:
         click_num += 1
-        #클릭한 이미지 찾기
+        # 클릭한 이미지 찾기
         card_idx = find_card(x, y)
         turtles[card_idx].shape(img_list[card_idx])
 
-        if click_num == 1: #첫번째 선택
+        if click_num == 1:  # 첫번째 선택
             first_pick = card_idx
         elif click_num == 2:
             second_pick = card_idx
-            click_num = 0 #반복되도록 0으로 초기화
+            click_num = 0  # 반복되도록 0으로 초기화
             attempt += 1
 
             if img_list[first_pick] == img_list[second_pick]:
                 score += 1
-                #정답
-                score_updata("정답")
+                # 정답
+                score_update("정답")
                 if score == 8:
                     result("성공")
                     player_list.append((attempt, nickname))
                     print((attempt, nickname))
-                    #show_scoreboard()
+                    show_scoreboard()
                     check_more_game()
             else:
-                score_updata("오답")
+                score_update("오답")
                 turtles[first_pick].shape(default_img)
                 turtles[second_pick].shape(default_img)
 
+
 def check_more_game():
     # 게임 한번 더 진행할 것인지 질문
+    global turtles
+
     root = Tk()
+
     def btn_yes():
         # 게임 한번 더 진행
         root.destroy()
         t.reset()
-        start_game()
+        for tur in turtles:
+            tur.reset()
+        get_nickname()
+
     def btn_no():
-        label = Label(root, text="게임이 종료되었습니다", width=300)
+        label = Label(root, text="게임이 종료되었습니다.", width=300)
         label.pack()
+        t.bye()
+
     root.geometry("300x70")
     root.title("한 게임 더❓ ")
     frame = tkinter.Frame(root)
@@ -89,6 +113,7 @@ def check_more_game():
     btn_yes.pack(side="right")
     root.mainloop()
 
+
 def get_nickname():
     def button_click():
         global nickname
@@ -96,6 +121,7 @@ def get_nickname():
         print("닉네임: " + nickname)
         window.destroy()
         start_game()
+
     window = Tk()
     window.geometry("250x70")
     window.title("닉네임 입력")
@@ -109,6 +135,7 @@ def get_nickname():
     button.pack()
     window.mainloop()
 
+
 def start_game():
     global turtles
     global img_list
@@ -119,8 +146,9 @@ def start_game():
     global second_pick
     global score_pen
 
+    turtles.clear()
+
     # turtle 객체 생성
-    t.bgcolor("white")
     t.setup(700, 700)
     t.up()
     t.ht()
@@ -166,45 +194,34 @@ def start_game():
     time.sleep(3)
     for i in range(16):  # 3초 후 default 이미지로 변경
         turtles[i].shape(default_img)
-    t.onscreenclick(play) #게임 시작
+    t.onscreenclick(play)  # 게임 시작
     t.done()  # 그래픽 창이 자동으로 닫히지 않도록
 
 
 default_img = "images/default_img.gif"
 game_title_img = "images/card_game_image.gif"
-nickname = "" #player nickname
-player_list = [] #튜플(시도 횟수, 닉네임) 형태로 저장
+nickname = ""  # player nickname
+player_list = []  # 튜플(시도 횟수, 닉네임) 형태로 저장
 
 pos_x = [-210, -70, 70, 210]
 pos_y = [-250, -110, 30, 170]
 
-
 turtles = []
 img_list = []
 
-#점수 펜 객체 생성
+# 점수 펜 객체 생성
 score_pen = t.Turtle()
 score_pen.up()
 score_pen.ht()
 score_pen.goto(0, 230)
 
-#turtle 객체 생성
-t.bgcolor("white")
-t.setup(700, 700)
-t.up()
-t.ht()
-t.goto(0, 280)
-t.addshape(game_title_img)
-t.shape(game_title_img)
-t.stamp()
 
-nickname = "" #player 닉네임
-click_num = 0 #클릭 횟수 (매 2회 클릭마다 정답 체크)
-score = 0 #점수
-attempt = 0 #시도한 횟수
-first_pick = "" #첫 번째 클릭한 이미지
-second_pick = "" #두 번째 클릭한 이미지
+nickname = ""  # player 닉네임
+click_num = 0  # 클릭 횟수 (매 2회 클릭마다 정답 체크)
+score = 0  # 점수
+attempt = 0  # 시도한 횟수
+first_pick = ""  # 첫 번째 클릭한 이미지
+second_pick = ""  # 두 번째 클릭한 이미지
 
-#닉네임 입력받으며 게임 시작
+# 닉네임 입력받으며 게임 시작
 get_nickname()
-
